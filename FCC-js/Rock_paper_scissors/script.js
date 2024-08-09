41d8025309bc6bc97e39159a860d2fca03e26fd7
brianjfrom <47318321+brianjfrom@users.noreply.github.com>
2024-08-09 03:19:05 -0600

    completed RPS/ added new: music player

--- FCC-js/Rock_paper_scissors/script.js
+++ FCC-js/Rock_paper_scissors/script.js
@@ -33,10 +33,52 @@ function getRoundResults(userOption) {
 
 const playerScoreSpanElement = document.getElementById("player-score");
 const computerScoreSpanElement = document.getElementById("computer-score");
-const roundResultMsg = document.getElementById("results-msg");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
 
-function showResults() {
-
+function showResults(userOption) {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    playerScoreSpanElement.innerText = playerScore
+    computerScoreSpanElement.innerText = computerScore
+    
+    if (playerScore === 3) {
+        winnerMsgElement.innerText = "Player has won the game!";
+        optionsContainer.style.display = "none";
+        resetGameBtn.style.display = "block";
+    } else if (computerScore === 3) {
+        winnerMsgElement.innerText = "Computer has won the game!";
+        optionsContainer.style.display = "none";
+        resetGameBtn.style.display = "block";
+    }
 };
 
-showResults("Rock");
+function resetGame() {
+    playerScore = 0,
+    playerScoreSpanElement.innerText = playerScore;
+    computerScore = 0;
+    computerScoreSpanElement.innerText = computerScore;
+    resetGameBtn.style.display = "none";
+    optionsContainer.style.display = "block";
+    winnerMsgElement.innerText = '';
+    roundResultsMsg.innerText = '';
+
+};
+resetGameBtn.addEventListener("click", resetGame);
+
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+
+rockBtn.addEventListener("click", function () {
+  showResults("Rock");
+});
+
+paperBtn.addEventListener("click", function () {
+  showResults("Paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+  showResults("Scissors");
+});
