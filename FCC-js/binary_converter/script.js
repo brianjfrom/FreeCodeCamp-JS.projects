b82f9320efb60731d4cb7087ba63cea48d6f6003
brianjfrom <47318321+brianjfrom@users.noreply.github.com>
2024-08-26 04:37:54 -0600

    updated binary project

--- FCC-js/binary_converter/script.js
+++ FCC-js/binary_converter/script.js
@@ -2,8 +2,60 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+// first binary conversion:
+
+// const decimalToBinary = (input) => {
+//     const inputs = [];
+//     const quotients = [];
+//     const remainders = [];
+
+//     if (input === 0) {
+//         result.innerText = "0";
+//         return
+//     }
+
+//     while (input > 0) {
+//         const quotient = Math.floor(input / 2);
+//         const remainder = (input % 2);
+
+//         inputs.push(input);
+//         quotients.push(quotient);
+//         remainders.push(remainder);
+//         input = quotient;
+//     }
+//     console.log("Inputs: ", inputs);
+//     console.log("Quotients: ", quotients);
+//     console.log("Remainders: ", remainders);
+
+//     result.innerText = remainders.reverse().join("");
+// };
+
+const decimalToBinary = (input) => {
+    let binary = "";
+    result.innerText = binary;
+
+    if (input === 0) {
+        binary = "0";
+        return
+    }
+
+    while (input > 0) {
+        binary = (input % 2) + binary;
+        input = Math.floor(input / 2);
+        
+    }
+}
+
+
 const checkUserInput = () => {
-    console.log(numberInput.value)
+    
+    if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0 ) {
+        window.alert("Please Provide a decimal number greater than or equal to 0");
+        return
+    }
+    
+    decimalToBinary(parseInt(numberInput.value));
+    numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
