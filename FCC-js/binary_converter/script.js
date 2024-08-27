b1977698dc172fb2dd2c11fafd339dde84b1770d
brianjfrom <47318321+brianjfrom@users.noreply.github.com>
2024-08-27 04:09:39 -0600

    updated binary converter

--- FCC-js/binary_converter/script.js
+++ FCC-js/binary_converter/script.js
@@ -1,8 +1,36 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
+const animationContainer = document.getElementById("animation-container");
 
-// first binary conversion:
+const animationData = [
+    {
+        inputVal: 5,
+        marginTop: 300,
+        addElDelay: 1000,
+        msg: "decimalToBinary(5) returns '10' + 1 (5 % 2). Then it pops off the stack.",
+        showMsgDelay: 15000,
+        removeElDelay: 20000,
+    },
+    {
+        inputVal: 2,
+        marginTop: -200,
+        addElDelay: 1500,
+        msg: "decimalToBinary(2) returns '1' + 0 (2 % 2) and gives that value to the stack below. Then it pops off the stack.",
+        showMsgDelay: 10000,
+        removeElDelay: 15000,
+    },
+    {
+        inputVal: 1,
+        marginTop: -200,
+        addElDelay: 2000,
+        msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+        showMsgDelay: 5000,
+        removeElDelay: 10000,
+    }
+];
+
+// --------------- first binary conversion: -----------------------------
 
 // const decimalToBinary = (input) => {
 //     const inputs = [];
@@ -30,31 +58,87 @@ const result = document.getElementById("result");
 //     result.innerText = remainders.reverse().join("");
 // };
 
-const decimalToBinary = (input) => {
-    let binary = "";
-    result.innerText = binary;
+// ------------------------- second iteration of binary conversion -------------
 
-    if (input === 0) {
-        binary = "0";
-        return
-    }
+// const decimalToBinary = (input) => {
+//     let binary = "";
+//     result.innerText = binary;
 
-    while (input > 0) {
-        binary = (input % 2) + binary;
-        input = Math.floor(input / 2);
+//     if (input === 0) {
+//         binary = "0";
+//         return
+//     }
+
+//     while (input > 0) {
+//         binary = (input % 2) + binary;
+//         input = Math.floor(input / 2);
         
-    }
-}
+//     }
+// }
 
+// -------------------third iteration of binary conversion--------------
+
+const decimalToBinary = (input) => {
+    if (input === 0 || input === 1) {
+        return String(input);
+    } else {
+        return decimalToBinary(Math.floor(input / 2)) + (input % 2);
+    } 
+} 
+
+
+// // ----------- Show animation function example: -------------
+
+// const showAnimation = () => {
+//     setTimeout(() => {
+//         console.log("free")        
+//     }, 500);
+
+//     setTimeout(() => {
+//         console.log("Code")
+//     }, 1000);
+
+//     setTimeout(() => {
+//         console.log("Camp")
+//     }, 1500);
+    
+// };
+
+
+// ------ show animation function -----
+
+const showAnimation = () => {
+    result.innerText = "Call Stack Animation";
+
+    animationData.forEach((obj) => {
+        setTimeout(() => {
+            animationContainer.innerHTML += `
+            <p id="${obj.inputVal}" style="margin-top: ${obj.marginTop}px;" class="animation-frame">decimalToBinary(${obj.inputVal})</p>
+            `
+        }, obj.addElDelay);
+
+        setTimeout(() => {
+               document.getElementById(obj.inputVal);
+
+        }, obj.showMsgDelay);
+    });
+}
 
 const checkUserInput = () => {
     
-    if (!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0 ) {
+    const inputInt = parseInt(numberInput.value)
+    
+    if (!numberInput.value || isNaN(inputInt) || inputInt < 0 ) {
         window.alert("Please Provide a decimal number greater than or equal to 0");
         return
     }
+
+    if (inputInt === 5) {
+        showAnimation();
+        return
+    }
     
-    decimalToBinary(parseInt(numberInput.value));
+    result.textContent = decimalToBinary(inputInt);
     numberInput.value = "";
 };
 
