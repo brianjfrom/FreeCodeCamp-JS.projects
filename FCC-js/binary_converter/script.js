b3ab1ca3a5c9ea268c051b979edbaa42aa17f0da
brianjfrom <47318321+brianjfrom@users.noreply.github.com>
2024-08-27 21:18:03 -0600

    Finished Binary Converter

--- FCC-js/binary_converter/script.js
+++ FCC-js/binary_converter/script.js
@@ -118,9 +118,17 @@ const showAnimation = () => {
         }, obj.addElDelay);
 
         setTimeout(() => {
-               document.getElementById(obj.inputVal);
+            document.getElementById(obj.inputVal).textContent = obj.msg;
 
         }, obj.showMsgDelay);
+
+        setTimeout(() => {
+            document.getElementById(obj.inputVal).remove();
+        }, obj.removeElDelay);
+
+        setTimeout(() => {
+            result.textContent = decimalToBinary(5);
+        }, 20000);
     });
 }
 
