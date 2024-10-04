c4eed8af6e835cb336841fb290634ac0b11a65b7
Brian From <brianjfrom@gmail.com>
2024-10-04 03:43:23 -0600

    finished js-spreadsheet

--- FCC-js/js_spreadsheet/script.js
+++ FCC-js/js_spreadsheet/script.js
@@ -30,7 +30,17 @@ const spreadsheetFunctions = {
     sum,
     average,
     median,
-    even: nums => nums.filter(isEven); 
+    even: nums => nums.filter(isEven),
+    someeven: nums => nums.some(isEven),
+    everyeven: nums => nums.every(isEven),
+    firsttwo: nums => nums.slice(0, 2),
+    lasttwo: nums => nums.slice(-2),
+    has2: nums => nums.includes(2),
+    increment: nums => nums.map(num => num + 1),
+    random: ([x, y]) => Math.floor(Math.random() * y + x),
+    range: ([x, y]) => range(x, y),
+    nodupes: nums => [...new Set(nums).values()],
+    "": nums => nums,
 };
 
 const applyFunction = str => {
