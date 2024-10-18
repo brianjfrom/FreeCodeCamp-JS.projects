afad2300d19a19a52a8aa2772e01a1b426b224eb
Brian From <brianjfrom@gmail.com>
2024-10-17 23:56:41 -0600

    update platform_game 10/17 2356

--- FCC-js/platform_game/script.js
+++ FCC-js/platform_game/script.js
@@ -10,6 +10,7 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 const gravity = 0.5;
 let isCheckpointCollisionDetectionActive = true;
+
 const proportionalSize = size => {
     return innerHeight < 500  ? Math.ceil((size / 500) * innerHeight) : size;
 };
@@ -39,7 +40,7 @@ class Player {
       
       if (this.position.y + this.height.y + this.velocity.y <= canvas.height) {
 
-          if (this.position.y < 0){
+          if (this.position.y < 0) {
             this.position.y = 0;
             this.velocity.y = gravity;
           }
@@ -47,6 +48,7 @@ class Player {
       } else {
         this.velocity.y = 0;
       }
+
       if (this.position.x < this.width) {
         this.position.x = this.width
       }
@@ -62,23 +64,72 @@ class Platform {
       x, y
     };
     this.width = 200;
+    this.height = proportionalSize(40);
+  }
+  draw() {
+    ctx.fillStyle = "#acd157";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 };
 
 const player = new Player()
 
+const platformPositions = [
+  {x: 500, y: proportionalSize(450)},
+  {x: 700, y: proportionalSize(400)},
+  { x: 850,  y: proportionalSize(350) },
+  { x: 900,  y: proportionalSize(350) },
+  { x: 1050, y: proportionalSize(150) },
+  { x: 2500, y: proportionalSize(450) },
+  { x: 2900, y: proportionalSize(400) },
+  { x: 3150, y: proportionalSize(350) },
+  { x: 3900, y: proportionalSize(450) },
+  { x: 4200, y: proportionalSize(400) },
+  { x: 4400, y: proportionalSize(200) },
+  { x: 4700, y: proportionalSize(150) }
+];
+
+const platforms = platformPositions.map(platform => new Platform(platform.x, platform.y));
+
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  platforms.forEach(platform => platform.draw());
+
   player.update();
+
   if (keys.rightKey.pressed && player.position.x < proportionalSize(400)) {
     player.velocity.x = 5;
   } else if (keys.leftKey.pressed && player.position.x > proportionalSize(100)) {
     player.velocity.x = -5;
   } else {
     player.velocity.x = 0;
-  }
+    
+    if (keys.rightKey.pressed && isCheckpointCollisionDetectionActive) {
+      platforms.forEach(platform => platform.position.x -= 5)
+    } else if (keys.leftKey.pressed && isCheckpointCollisionDetectionActive) {
+      platforms.forEach(platform => platform.position.x += 5)
+    };
+  } 
+
+platforms.forEach(platform => {
+  const collisionDetectionRules = [
+    player.position.y + player.height <= platform.position.y,
+    player.position.y + player.height + player.velocity.y >= platform.position.y,
+    player.position.x >= platform.position.x - (player.width / 2),
+    player.position.x <= platform.position.x + platform.width - (player.width / 3) // check this later?
+  ];
+
+  if (collisionDetectionRules.every(rule => rule)) {
+    player.velocity.y = 0;
+    return
+  };
+  const platformDetectionRules = [];
+});
+
 };
+
 
 const keys = {
   rightKey: {
