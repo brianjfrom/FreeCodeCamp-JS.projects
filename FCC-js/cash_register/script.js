490936ee88318a3e605ce2a1c6b143cc6e45ba70
Brian From <brianjfrom@gmail.com>
2024-11-03 00:22:57 -0600

    update to cash-register 11.3 0022

--- FCC-js/cash_register/script.js
+++ FCC-js/cash_register/script.js
@@ -1,4 +1,4 @@
-let price = 1.87
+let price = 3.69
 let cid = [
     ['PENNY', 1.01],
     ['NICKEL', 2.05],
@@ -12,8 +12,8 @@ let cid = [
 ];
 
 const cash = document.getElementById('cash');
-const change = getElementById('change-due');
-const purchaseBtn = getElementById('purchase-btn');
+const change = document.getElementById('change-due');
+const purchaseBtn = document.getElementById('purchase-btn');
 
 let currencyUnits = [
     ['PENNY', .01],
@@ -33,5 +33,53 @@ purchaseBtn.addEventListener("click", () => {
 
     if (cashValue < price) {
         alert("Customer does not have enough mony to purchase this item");
+        return
     }
-});
+    if (cashValue === price) {
+        change.innerText = "No change due - customer paid with exact cash"
+    }
+}); 
+
+const getChange = (changeDue, cid) => {
+    let totalCid = parseFloat(cid.reduce((sum, [_,amount]) => sum + amount, 0).toFixed(2));
+    if (totalCid < changeDue) {
+        return { Status: "INSUFFICIENT_FUNDS", change: [] };
+    }
+
+    let changeArray = [];
+    let remainingChange = changeDue;
+
+    for (let i = currencyUnits.length - 1; i >= 0; i--) {
+        let unit = currencyUnits[i][0];
+        let unitValue = currencyUnits[i][1];
+        let unitInDrawer = cid[i][1];
+
+        if (unitValue <= remainingChange && unitInDrawer > 0) {
+            let amountFromUnit = 0;
+
+            while (remainingChange >= unitValue && unitInDrawer > 0) {
+                remainingChange = (remainingChange - unitValue).toFixed(2);
+                unitInDrawer -= unitValue;
+                amountFromUnit += unitValue;
+            }
+            if (amountFromUnit > 0) {
+                changeArray.push([unit, amountFromUnit]);
+            }
+        }
+    } // end of for loop
+
+    if (remainingChange > 0) {
+        return { Status: "INSUFFICIENT_FUNDS", change: [] };
+    }
+
+    if (changeDue === totalCid) {
+        return { Status: "CLOSED", change: cid }
+    }
+
+    return { Status: "OPEN", change: changeArray };
+
+} // end of getChange()
+
+
+
+console.log(getChange(5, cid))
